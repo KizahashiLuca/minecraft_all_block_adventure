e54e58aeecfbd88c6330ef97e7acbffbb30e7622
KizahashiLuca <kizahashi.luca.25@gmail.com>
2022-11-22 15:12:14 +0900

    [Updated] sorting

--- data/maa/functions/start.mcfunction
+++ data/maa/functions/start.mcfunction
@@ -16,6 +16,64 @@ gamemode adventure @a
 ## Revoke advancements
 advancement revoke @a everything
 advancement grant @a only maa:blocks
+advancement grant @a only maa:blocks/1_end
+advancement grant @a only maa:blocks/2_end
+advancement grant @a only maa:blocks/3_end
+advancement grant @a only maa:blocks/4_end
+advancement grant @a only maa:blocks/5_end
+advancement grant @a only maa:blocks/6_end
+advancement grant @a only maa:blocks/7_end
+advancement grant @a only maa:blocks/8_end
+advancement grant @a only maa:blocks/9_end
+advancement grant @a only maa:blocks/10_end
+advancement grant @a only maa:blocks/11_end
+advancement grant @a only maa:blocks/12_end
+advancement grant @a only maa:blocks/13_end
+advancement grant @a only maa:blocks/14_end
+advancement grant @a only maa:blocks/15_end
+advancement grant @a only maa:blocks/16_end
+advancement grant @a only maa:blocks/17_end
+advancement grant @a only maa:blocks/18_end
+advancement grant @a only maa:blocks/19_end
+advancement grant @a only maa:blocks/20_end
+advancement grant @a only maa:blocks/21_end
+advancement grant @a only maa:blocks/22_end
+advancement grant @a only maa:blocks/23_end
+advancement grant @a only maa:blocks/24_end
+advancement grant @a only maa:blocks/25_end
+advancement grant @a only maa:blocks/26_end
+advancement grant @a only maa:blocks/27_end
+advancement grant @a only maa:blocks/28_end
+advancement grant @a only maa:blocks/29_end
+advancement grant @a only maa:blocks/30_end
+advancement grant @a only maa:blocks/31_end
+advancement grant @a only maa:blocks/32_end
+advancement grant @a only maa:blocks/33_end
+advancement grant @a only maa:blocks/34_end
+advancement grant @a only maa:blocks/35_end
+advancement grant @a only maa:blocks/36_end
+advancement grant @a only maa:blocks/37_end
+advancement grant @a only maa:blocks/38_end
+advancement grant @a only maa:blocks/39_end
+advancement grant @a only maa:blocks/40_end
+advancement grant @a only maa:blocks/41_end
+advancement grant @a only maa:blocks/42_end
+advancement grant @a only maa:blocks/43_end
+advancement grant @a only maa:blocks/44_end
+advancement grant @a only maa:blocks/45_end
+advancement grant @a only maa:blocks/46_end
+advancement grant @a only maa:blocks/47_end
+advancement grant @a only maa:blocks/48_end
+advancement grant @a only maa:blocks/49_end
+advancement grant @a only maa:blocks/50_end
+advancement grant @a only maa:blocks/51_end
+advancement grant @a only maa:blocks/52_end
+advancement grant @a only maa:blocks/53_end
+advancement grant @a only maa:blocks/54_end
+advancement grant @a only maa:blocks/55_end
+advancement grant @a only maa:blocks/56_end
+advancement grant @a only maa:blocks/57_end
+advancement grant @a only maa:blocks/57_end
 
 ## Set spawn
 setworldspawn ~ ~ ~
--- set_block_list.sh
+++ set_block_list.sh
@@ -11,8 +11,11 @@
 
 ## Set path
 BlockList=./block_list.csv
+declare -a OverworldArray=()
+declare -a NetherArray=()
+declare -a EndArray=()
 declare -a ColorArray=("white" "light_gray" "gray" "black" "brown" "red" "orange" "yellow" "lime" "green" "cyan" "light_blue" "blue" "purple" "magenta" "pink")
-declare -a NumberArray=("50" "51" "52" "53" "54" "55" "56" "57" "4" "5" "2" "3" "1" "8" "9" "6" "7" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "43" "44" "45" "46" "47" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "48" "49")
+declare -a NumberArray=("53" "54" "55" "56" "57" "4" "5" "2" "3" "1" "8" "9" "6" "7" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" "41" "42" "43" "44" "50" "51" "52" "45" "46" "47" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "48" "49")
 
 ## Remove existing files
 rm ./data/maa/advancements/blocks/*
@@ -26,12 +29,41 @@ while read R; do
   col[0]=`echo ${R} | cut -d , -f 1`
   col[1]=`echo ${R} | cut -d , -f 2`
   col[2]=`echo ${R} | cut -d , -f 3`
+  col[3]=`echo ${R} | cut -d , -f 4`
 
   ## Skip row
   if [ "${col[0]}" = "" ]; then
+    if [ $i != 0 ]; then
+      ## Write advancements json
+      AdvancementsJson=./data/maa/advancements/blocks/${NumberArray[$j]}_end.json
+      parent=blocks/${ItemArray[$i-1]}
+      cat << EOS > ${AdvancementsJson}
+{
+  "parent": "maa:${parent}",
+  "display": {
+    "icon": {
+      "item": "minecraft:structure_void"
+    },
+    "title": "",
+    "description": "",
+    "announce_to_chat": false,
+    "frame": "task",
+    "hidden": false,
+    "show_toast": false
+  },
+  "criteria": {
+    "${NumberArray[$j]}_end": {
+      "trigger": "minecraft:impossible"
+    }
+  },
+  "requirements": [
+    ["${NumberArray[$j]}_end"]
+  ]
+}
+EOS
+    fi
     i=0
     j=$((j+1))
-    echo $j
     declare -a ItemArray=()
     continue;
   fi
@@ -43,18 +75,35 @@ while read R; do
     continue;
   fi
 
-  ## Add lines
+  ## Add to arrays
   if [ $i == 0 ]; then
     AdvancementsJson=./data/maa/advancements/blocks/${NumberArray[$j]}.json
-    parent=blocks
-  elif [ $i == 1 ]; then
-    AdvancementsJson=./data/maa/advancements/blocks/${ItemArray[$i]}.json
-    parent=blocks/${NumberArray[$j]}
+    if [ "${col[2]}" = "x" ]; then
+      parent=nether
+    elif [ "${col[3]}" = "x" ]; then
+      parent=end
+    else
+      parent=overworld
+    fi
   else
     AdvancementsJson=./data/maa/advancements/blocks/${ItemArray[$i]}.json
-    parent=blocks/${ItemArray[$i-1]}
+    if [ $i == 1 ]; then
+      parent=blocks/${NumberArray[$j]}
+    else
+      parent=blocks/${ItemArray[$i-1]}
+    fi
   fi
-  echo ${col[0]} ${col[1]} ${ItemArray[$i]}
+
+  ## Add to arrays
+  if [ "${col[2]}" = "x" ] && [ "${col[3]}" = "" ]; then
+    NetherArray+=( "${ItemArray[$i]}" )
+  elif [ "${col[2]}" = "" ] && [ "${col[3]}" = "x" ]; then
+    EndArray+=( "${ItemArray[$i]}" )
+  elif [ "${col[2]}" = "" ] && [ "${col[3]}" = "" ]; then
+    OverworldArray+=( "${ItemArray[$i]}" )
+  fi
+
+  ## Write advancements json
   cat << EOS > ${AdvancementsJson}
 {
   "parent": "maa:${parent}",
@@ -94,6 +143,7 @@ while read R; do
 }
 EOS
 
+  ## Write function
   Mcfunction=./data/maa/functions/blocks/${ItemArray[$i]}.mcfunction
   cat << EOS > ${Mcfunction}
 ##################################
@@ -115,3 +165,180 @@ EOS
   ## Increment
   i=$((i+1))
 done < ${BlockList}
+
+## Overworld advancements
+AdvancementsJson=./data/maa/advancements/overworld.json
+cat << EOS > ${AdvancementsJson}
+{
+  "parent": "maa:blocks",
+  "display": {
+    "icon": {
+      "item": "minecraft:grass_block"
+    },
+    "title": {
+      "translate": "オーバーワールド"
+    },
+    "description": "",
+    "announce_to_chat": false,
+    "frame": "challenge",
+    "hidden": false,
+    "show_toast": false
+  },
+  "criteria": {
+EOS
+for i in "${OverworldArray[@]}"; do
+  cat << EOS >> ${AdvancementsJson}
+    "$i": {
+      "trigger": "minecraft:inventory_changed",
+      "conditions": {
+        "items": [
+          {
+            "items": [
+              "minecraft:$i"
+            ]
+          }
+        ]
+      }
+    },
+EOS
+done
+sed -i -e '$d' ${AdvancementsJson}
+cat << EOS >> ${AdvancementsJson}
+    }
+  },
+  "requirements": [
+EOS
+for i in "${OverworldArray[@]}"; do
+  cat << EOS >> ${AdvancementsJson}
+    [
+      "$i"
+    ],
+EOS
+done
+sed -i -e '$d' ${AdvancementsJson}
+cat << EOS >> ${AdvancementsJson}
+    ]
+  ],
+  "rewards": {
+    "function": "maa:overworld"
+  }
+}
+EOS
+
+## Nether advancements
+AdvancementsJson=./data/maa/advancements/nether.json
+cat << EOS > ${AdvancementsJson}
+{
+  "parent": "maa:blocks",
+  "display": {
+    "icon": {
+      "item": "minecraft:netherrack"
+    },
+    "title": {
+      "translate": "ネザー"
+    },
+    "description": "",
+    "announce_to_chat": false,
+    "frame": "challenge",
+    "hidden": false,
+    "show_toast": false
+  },
+  "criteria": {
+EOS
+for i in "${NetherArray[@]}"; do
+  cat << EOS >> ${AdvancementsJson}
+    "$i": {
+      "trigger": "minecraft:inventory_changed",
+      "conditions": {
+        "items": [
+          {
+            "items": [
+              "minecraft:$i"
+            ]
+          }
+        ]
+      }
+    },
+EOS
+done
+sed -i -e '$d' ${AdvancementsJson}
+cat << EOS >> ${AdvancementsJson}
+    }
+  },
+  "requirements": [
+EOS
+for i in "${NetherArray[@]}"; do
+  cat << EOS >> ${AdvancementsJson}
+    [
+      "$i"
+    ],
+EOS
+done
+sed -i -e '$d' ${AdvancementsJson}
+cat << EOS >> ${AdvancementsJson}
+    ]
+  ],
+  "rewards": {
+    "function": "maa:nether"
+  }
+}
+EOS
+
+## End advancements
+AdvancementsJson=./data/maa/advancements/end.json
+cat << EOS > ${AdvancementsJson}
+{
+  "parent": "maa:blocks",
+  "display": {
+    "icon": {
+      "item": "minecraft:end_stone"
+    },
+    "title": {
+      "translate": "エンド"
+    },
+    "description": "",
+    "announce_to_chat": false,
+    "frame": "challenge",
+    "hidden": false,
+    "show_toast": false
+  },
+  "criteria": {
+EOS
+for i in "${EndArray[@]}"; do
+  cat << EOS >> ${AdvancementsJson}
+    "$i": {
+      "trigger": "minecraft:inventory_changed",
+      "conditions": {
+        "items": [
+          {
+            "items": [
+              "minecraft:$i"
+            ]
+          }
+        ]
+      }
+    },
+EOS
+done
+sed -i -e '$d' ${AdvancementsJson}
+cat << EOS >> ${AdvancementsJson}
+    }
+  },
+  "requirements": [
+EOS
+for i in "${EndArray[@]}"; do
+  cat << EOS >> ${AdvancementsJson}
+    [
+      "$i"
+    ],
+EOS
+done
+sed -i -e '$d' ${AdvancementsJson}
+cat << EOS >> ${AdvancementsJson}
+    ]
+  ],
+  "rewards": {
+    "function": "maa:end"
+  }
+}
+EOS
